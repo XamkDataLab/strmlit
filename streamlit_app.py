18ed586dcf02da473da86dff60c3d96e6f6056ba
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-09-11 21:38:19 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,81 +1,34 @@
-import streamlit as st
-import plotly.graph_objects as go
+from collections import namedtuple
 import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
 """
-# Aineettoman omaisuuden Suomi
+# Welcome to Streamlit!
 
-Alueiden innovaatiotoiminnan kirjaamo
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart: ::P:
 
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
 
+In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
 
 def main():
-    st.title("Crude Representation of Finland with Adjustable Borders")
+    st.title("Simple Scatter Plot with Plotly and Streamlit")
 
-    # Function to bend the borders based on slider values
-    def update_coords(top_bend, bottom_bend):
-        return [0, 1+bottom_bend, 1+top_bend, 0, 0], [0, 0, 1, 1, 0]
+    # Sample data
+    df = {
+        'x': [1, 2, 3, 4, 5],
+        'y': [2, 3, 4, 5, 6]
+    }
 
-    # Initial coordinates for the crude representation of Finland
-    x = [0, 1, 1, 0, 0]
-    y = [0, 0, 1, 1, 0]
+    # Create a scatter plot
+    fig = px.scatter(df, x='x', y='y', title='Simple Scatter Plot')
 
-    # Create the plot
-    fig = go.Figure()
-
-    fig.add_trace(go.Scatter(x=x, y=y, fill='toself', mode='lines'))
-
-    # Add sliders
-    sliders = [
-        # Slider for top bend
-        {
-            'active': 0,
-            'yanchor': 'top',
-            'xanchor': 'left',
-            'currentvalue': {
-                'font': {'size': 20},
-                'prefix': 'Top Bend: ',
-                'visible': True,
-                'xanchor': 'right'
-            },
-            'transition': {'duration': 300, 'easing': 'cubic-in-out'},
-            'pad': {'b': 10, 't': 50},
-            'len': 0.9,
-            'x': 0.1,
-            'y': 0,
-            'steps': [{
-                'args': [[x, *update_coords(val*0.1, (1-val)*0.1)], {'frame': {'duration': 300, 'redraw': True}, 'mode': 'immediate'}],
-                'label': str(val),
-                'method': 'restyle'
-            } for val in range(10)]
-        },
-        # Slider for bottom bend
-        {
-            'active': 0,
-            'yanchor': 'top',
-            'xanchor': 'left',
-            'currentvalue': {
-                'font': {'size': 20},
-                'prefix': 'Bottom Bend: ',
-                'visible': True,
-                'xanchor': 'right'
-            },
-            'transition': {'duration': 300, 'easing': 'cubic-in-out'},
-            'pad': {'b': 10, 't': 50},
-            'len': 0.9,
-            'x': 0.1,
-            'y': 0.1,
-            'steps': [{
-                'args': [[x, *update_coords((1-val)*0.1, val*0.1)], {'frame': {'duration': 300, 'redraw': True}, 'mode': 'immediate'}],
-                'label': str(val),
-                'method': 'restyle'
-            } for val in range(10)]
-        }
-    ]
-
-    fig.update_layout(sliders=sliders)
-    
     # Display the plot in Streamlit
     st.plotly_chart(fig)
 
