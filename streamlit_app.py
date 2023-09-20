ed42f49f65e1b8c0d81190aa6dffbc6d7418b110
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-09-20 13:00:31 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pyodbc
 import streamlit as st
+from queries import * 
 
 driver = st.secrets["driver"]
 server = st.secrets["server"]
@@ -8,87 +9,6 @@ database = st.secrets["database"]
 username = st.secrets["username"]
 password = st.secrets["password"]
 
-def fetch_data(y_tunnus):
-    # Define the SQL query
-    query = """
-    WITH Funding AS (
-        SELECT 
-            Y_tunnus,
-            SUM(Toteutunut_EU_ja_valtion_rahoitus) as Total_Funding
-        FROM 
-            eura2020
-        WHERE 
-            Y_tunnus = ?
-        GROUP BY 
-            Y_tunnus
-    ),
-    DesignRights AS (
-        SELECT 
-            m.applicant_basename,
-            COUNT(DISTINCT m.applicationNumber) as Design_Rights_Count
-        FROM 
-            mallioikeudet m
-        JOIN 
-            yritykset y ON y.yritys_basename = m.applicant_basename
-        WHERE 
-            y.y_tunnus = ?
-        GROUP BY 
-            m.applicant_basename
-    ),
-    Trademarks AS (
-        SELECT 
-            t.applicant_basename,
-            COUNT(DISTINCT t.applicationNumber) as Trademarks_Count
-        FROM 
-            tavaramerkit t
-        JOIN 
-            yritykset y ON y.yritys_basename = t.applicant_basename
-        WHERE 
-            y.y_tunnus = ?
-        GROUP BY 
-            t.applicant_basename
-    ),
-    Patents AS (
-        SELECT 
-            p.applicant_basename,
-            COUNT(DISTINCT p.lens_id) as Patent_Applications_Count
-        FROM 
-            applicants p
-        JOIN 
-            yritykset y ON y.yritys_basename = p.applicant_basename
-        WHERE 
-            y.y_tunnus = ?
-        GROUP BY 
-            p.applicant_basename
-    )
-
-    SELECT 
-        y.y_tunnus,
-        y.yritys,
-        y.yritys_basename,
-        COALESCE(f.Total_Funding, 0) as Total_Funding,
-        COALESCE(d.Design_Rights_Count, 0) as Design_Rights_Count,
-        COALESCE(t.Trademarks_Count, 0) as Trademarks_Count,
-        COALESCE(p.Patent_Applications_Count, 0) as Patent_Applications_Count
-    FROM 
-        yritykset y
-    LEFT JOIN 
-        Funding f ON y.y_tunnus = f.Y_tunnus
-    LEFT JOIN 
-        DesignRights d ON y.yritys_basename = d.applicant_basename
-    LEFT JOIN 
-        Trademarks t ON y.yritys_basename = t.applicant_basename
-    LEFT JOIN 
-        Patents p ON y.yritys_basename = p.applicant_basename
-    WHERE 
-        y.y_tunnus = ?;
-    """
-    
-    # Connect to the database and fetch the data into a Pandas DataFrame
-    with pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}') as conn:
-        df = pd.read_sql(query, conn, params=(y_tunnus, y_tunnus, y_tunnus, y_tunnus, y_tunnus))
-        
-    return df
 # Define custom styles
 small_font_style = """
 <style>
@@ -122,18 +42,14 @@ large_number_style = """
 </style>
 """
 
-# Inject custom styles into the app
 st.markdown(small_font_style, unsafe_allow_html=True)
 st.markdown(medium_font_style, unsafe_allow_html=True)
 st.markdown(large_font_style, unsafe_allow_html=True)
 st.markdown(large_number_style, unsafe_allow_html=True)
 
 st.title('Hae yrityksen tiedot')
-
-# Input for Y_tunnus
 y_tunnus = st.text_input("Anna Y-tunnus (ja paina enter)")
 
-# If a Y_tunnus is given, fetch and display the data
 if y_tunnus:
     data = fetch_data(y_tunnus)
     #st.write("Debug: Fetched data:")
