4312e7c82471c3d8c2ba671e58c7bfcec59a9cf8
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-09-21 08:34:20 +0300

    Update queries.py

--- queries.py
+++ queries.py
@@ -60,28 +60,61 @@ def fetch_data(y_tunnus):
             y.y_tunnus = ?
         GROUP BY 
             p.applicant_basename
+    ),
+    EUHorizon AS (
+    SELECT 
+        beneficiary_basename,
+        SUM([Beneficiary’s contracted amount (EUR)]) as Total_EU_Horizon_Funding
+    FROM 
+        EU_Horizon
+
+    JOIN yritykset y on y.yritys_basename = EU_Horizon.beneficiary_basename
+
+    WHERE 
+        y.y_tunnus = ?
+    GROUP BY 
+        EU_Horizon.beneficiary_basename
+    ),
+    BusinessFinland AS (
+    SELECT 
+        Business_Finland.Y_tunnus,
+        SUM(CAST(Avustus as FLOAT)) as Total_Business_Finland_Funding
+    FROM 
+        Business_Finland
+
+    JOIN yritykset y on y.y_tunnus = Business_Finland.Y_tunnus
+    WHERE 
+        y.y_tunnus = ?
+    GROUP BY 
+        Business_Finland.Y_tunnus
     )
 
     SELECT 
-        y.y_tunnus,
-        y.yritys,
-        y.yritys_basename,
-        COALESCE(f.Total_Funding, 0) as Total_Funding,
-        COALESCE(d.Design_Rights_Count, 0) as Design_Rights_Count,
-        COALESCE(t.Trademarks_Count, 0) as Trademarks_Count,
-        COALESCE(p.Patent_Applications_Count, 0) as Patent_Applications_Count
+    y.y_tunnus,
+    y.yritys,
+    y.yritys_basename,
+    COALESCE(f.Total_Funding, 0) as Total_Funding,
+    COALESCE(d.Design_Rights_Count, 0) as Design_Rights_Count,
+    COALESCE(t.Trademarks_Count, 0) as Trademarks_Count,
+    COALESCE(p.Patent_Applications_Count, 0) as Patent_Applications_Count,
+    COALESCE(eh.Total_EU_Horizon_Funding, 0) as Total_EU_Horizon_Funding,
+    COALESCE(bf.Total_Business_Finland_Funding, 0) as Total_Business_Finland_Funding
     FROM 
-        yritykset y
+    yritykset y
     LEFT JOIN 
-        Funding f ON y.y_tunnus = f.Y_tunnus
+    Funding f ON y.y_tunnus = f.Y_tunnus
     LEFT JOIN 
-        DesignRights d ON y.yritys_basename = d.applicant_basename
+    DesignRights d ON y.yritys_basename = d.applicant_basename
     LEFT JOIN 
-        Trademarks t ON y.yritys_basename = t.applicant_basename
+    Trademarks t ON y.yritys_basename = t.applicant_basename
     LEFT JOIN 
-        Patents p ON y.yritys_basename = p.applicant_basename
+    Patents p ON y.yritys_basename = p.applicant_basename
+    LEFT JOIN 
+    EUHorizon eh ON y.yritys_basename = eh.beneficiary_basename
+    LEFT JOIN 
+    BusinessFinland bf ON y.y_tunnus = bf.Y_tunnus
     WHERE 
-        y.y_tunnus = ?;
+    y.y_tunnus = ?;
     """
     
     # Connect to the database and fetch the data into a Pandas DataFrame
