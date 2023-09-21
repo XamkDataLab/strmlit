b61b1b9fa70c6a079ca5ab4f8b6da310d8aa815e
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-09-21 15:07:09 +0300

    Update detailed_info.py

--- pages/detailed_info.py
+++ pages/detailed_info.py
@@ -1,4 +1,20 @@
 import streamlit as st
 
 st.title(f"Details for {y_tunnus}")
-y_tunnus = st.session_state['y_tunnus'] 
+# detailed_info.py
+
+import streamlit as st
+
+def app():
+    y_tunnus = st.session_state.get('y_tunnus', None)
+    query_type = st.session_state.get('type', None)
+
+    if y_tunnus and query_type:
+        data = another_fetch_data_function(y_tunnus, query_type)
+        
+        if not data.empty:
+            # Code to display data and visualization
+            ...
+        else:
+            st.write("No data found.")
+
