6d6a8f146bc68aecc466822d823e6a14bfaa9aea
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-09-22 10:26:49 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,11 +45,14 @@ st.title('Hae yrityksen tiedot')
 # Input for Y_tunnus
 y_tunnus = st.text_input("Anna Y-tunnus (ja paina enter)")
 st.session_state['y_tunnus'] = y_tunnus
+
+# Function to format numbers with space as thousands separator and add € symbol
+def format_currency(number):
+    return f"{number:,.0f} €".replace(",", " ")
+
 # If a Y_tunnus is given, fetch and display the data
 if y_tunnus:
     data = fetch_data(y_tunnus)
-    #st.write("Debug: Fetched data:")
-    #st.write(data)
 
     if not data.empty:
         st.markdown(f"<div class='large-font'>{data['yritys'].iloc[0]}</div>", unsafe_allow_html=True)
@@ -57,20 +60,19 @@ if y_tunnus:
         col1, col2 = st.columns(2)  # Create two columns
     
         # Content for the first column
-        # Content for the first column
         card_content1 = f"""
         <div style="background-color: #f5f5f5; padding: 15px; border-radius: 5px;">
         <div class="medium-font">EU Horizon rahoitus 2013-2030</div>
-        <div class="large-number">{int(data['Total_EU_Horizon_Funding'].iloc[0]):,}</div>
+        <div class="large-number">{format_currency(int(data['Total_EU_Horizon_Funding'].iloc[0]))}</div>
         <div class="small-font"><a href="/detailed_info?y_tunnus={y_tunnus}&type=Total_EU_Horizon_Funding">linkki tarkempiin tietoihin (hankkeet ja ohjelmat)</a></div>
         <hr>
         <div class="medium-font">EURA-rahoitus 2014-2020 ohjelmakausi</div>
-        <div class="large-number">{int(data['Total_Funding'].iloc[0]):,} €</div>
+        <div class="large-number">{format_currency(int(data['Total_Funding'].iloc[0]))}</div>
         <div class="small-font">2021-2027 ohjelmakauden tietolähde julkaistaan lokakuun alussa</div>
         <div class="small-font"><a href="/detailed_info?y_tunnus={y_tunnus}&type=Total_Funding">linkki tarkempiin tietoihin (esim. isoimmat hankkeet)</a></div>
         <hr>
         <div class="medium-font">Business Finland tuet</div>
-        <div class="large-number">{int(data['Total_Business_Finland_Funding'].iloc[0]):,}</div>
+        <div class="large-number">{format_currency(int(data['Total_Business_Finland_Funding'].iloc[0]))}</div>
         <div class="small-font"><a href="/detailed_info?y_tunnus={y_tunnus}&type=Total_Business_Finland_Funding">linkki tarkempiin tietoihin</a></div>
         </div>
         """
@@ -80,20 +82,19 @@ if y_tunnus:
         card_content2 = f"""
         <div style="background-color: #f5f5f5; padding: 15px; border-radius: 5px;">
         <div class="medium-font">Patenttien määrä</div>
-        <div class="large-number">{int(data['Patent_Applications_Count'].iloc[0]):,}</div>
+        <div class="large-number">{format_currency(int(data['Patent_Applications_Count'].iloc[0]))}</div>
         <div class="small-font"><a href="/detailed_info?y_tunnus={y_tunnus}&type=Patent_Applications_Count">linkki tarkempiin tietoihin (patenttilistaus + visualisoinnit)</a></div>
         <hr>
         <div class="medium-font">Tavaramerkkien määrä</div>
-        <div class="large-number">{int(data['Trademarks_Count'].iloc[0]):,}</div>
+        <div class="large-number">{format_currency(int(data['Trademarks_Count'].iloc[0]))}</div>
         <div class="small-font"><a href="/detailed_info?y_tunnus={y_tunnus}&type=Trademarks_Count">linkki tarkempiin tietoihin (sanat & kuvat?)</a></div>
         <hr>
         <div class="medium-font">Mallioikeuksien määrä</div>
-        <div class="large-number">{int(data['Design_Rights_Count'].iloc[0]):,}</div>
+        <div class="large-number">{format_currency(int(data['Design_Rights_Count'].iloc[0]))}</div>
         <div class="small-font"><a href="/detailed_info?y_tunnus={y_tunnus}&type=Design_Rights_Count">linkki tarkempiin tietoihin</a></div>
         </div>
         """
         col2.markdown(card_content2, unsafe_allow_html=True)
-
 
     else:
         st.write("Dataa ei löytynyt :(")
