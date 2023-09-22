9a55814ad76a7e9ff8e8bc3d0a6b0dc98e2bd2b1
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-09-22 13:38:11 +0300

    Update EU_Horizon.py

--- pages/EU_Horizon.py
+++ pages/EU_Horizon.py
@@ -1,18 +1,10 @@
 import streamlit as st
+from queries import *
+
 y_tunnus = st.session_state.get('y_tunnus')
 st.title(f"EU Horizon funding for {y_tunnus}")
-# detailed_info.py
 
-
-def app():
-    y_tunnus = st.session_state.get('y_tunnus', 'Unknown')  # Get y_tunnus from session state
-    query_type = st.session_state.get('type', 'Unknown')  # Get query type from session state
-
-    st.title(f"Details for {y_tunnus}")
-
-    if y_tunnus != 'Unknown' and query_type != 'Unknown':
-        data = another_fetch_data_function(y_tunnus, query_type)
-        
+data = fetch_horizon_data(y_tunnus)
         if not data.empty:
             # Code to display data and visualization
             ...
