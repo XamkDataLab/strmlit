c4ffdc419371fa5a319d962ad1d7d69c2480ccbc
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-09-25 12:05:36 +0300

    Update EU_Horizon.py

--- pages/EU_Horizon.py
+++ pages/EU_Horizon.py
@@ -4,13 +4,13 @@ from queries import *
 y_tunnus = st.session_state.get('y_tunnus')
 st.title(f"EU Horizon funding for {y_tunnus}")
 
-data = fetch_horizon_data(y_tunnus)
-        if not data.empty:
-            # Code to display data and visualization
-            ...
-        else:
-            st.write("No data found.")
+if y_tunnus:
+    data = fetch_horizon_data(y_tunnus)
+    if not data.empty:
+         st.dataframe(data)
     else:
-        st.write("Invalid or missing parameters.")
+        st.write("No data found.")
+else:
+    st.write("Invalid or missing parameters.")
 
 
