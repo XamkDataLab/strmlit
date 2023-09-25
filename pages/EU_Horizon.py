c4314a664a2cf18ec8fa68860e521e0a51d5d2af
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-09-25 12:31:06 +0300

    Update EU_Horizon.py

--- pages/EU_Horizon.py
+++ pages/EU_Horizon.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from queries import *
 
-yritys_basename = st.session_state.get('yritys_basename')
+yritys_basename = st.session_state.get('yritys_basename2')
 st.title(f"EU Horizon funding for {yritys_basename}")
 st.write(f"Debug: yritys_basename = {yritys_basename}")
 
