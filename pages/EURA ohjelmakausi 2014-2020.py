87c4b817215e33cb4333cfcebad51853c0ffc52d
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-09-27 09:53:10 +0300

    Update EURA ohjelmakausi 2014-2020.py

--- pages/EURA ohjelmakausi 2014-2020.py
+++ pages/EURA ohjelmakausi 2014-2020.py
@@ -8,6 +8,7 @@ from queries import *
 def truncate_text(text, max_length):
     return text if len(text) <= max_length else text[:max_length] + "..."
 
+
 def generate_eura_project_viz(df, filter_ended=True):
     # Convert the start and end date columns to datetime format
     df['Aloituspvm'] = pd.to_datetime(df['Aloituspvm'])
@@ -26,24 +27,26 @@ def generate_eura_project_viz(df, filter_ended=True):
     df['Hover Info'] = 'Budget: ' + df['Myönnetty_EU_ja_valtion_rahoitus'].astype(str)
 
     # Create the Gantt chart using plotly
-    fig = px.timeline(df, x_start="Aloituspvm", x_end="Päättymispvm", y="Truncated Name", 
-                      color="Truncated Name", 
-                      hover_name="Hankekoodi", 
-                      hover_data=["Hover Info"], 
+    fig = px.timeline(df, x_start="Aloituspvm", x_end="Päättymispvm", y="Truncated Name",
+                      color="Truncated Name",
+                      hover_name="Hankkeen_nimi",
+                      hover_data=["Hover Info"],
                       title="EURA2020 Projects")
 
     fig.update_yaxes(categoryorder="total ascending")  # Sort projects based on start date
-    fig.update_traces(marker_line_width=df['Myönnetty_EU_ja_valtion_rahoitus']/500000)  # Set line width based on budget
+    # Set line width based on budget
+    fig.update_traces(marker_line_width=df['Myönnetty_EU_ja_valtion_rahoitus']/500000)
 
     # Remove the color legend
     fig.update_layout(showlegend=False)
-    
+
     # Hide the y-axis label
     fig.update_layout(yaxis_title_text="")
 
     # In a real Streamlit app, we would display the plot using st.plotly_chart(fig)
     st.plotly_chart(fig)
-    
+
+
 # Retrieve the yritys_basename from session state
 y_tunnus = st.session_state.get('y_tunnus')
 yritys_basename = st.session_state.get('yritys_basename2')
@@ -61,7 +64,7 @@ selected_toimintalinja = st.selectbox("Valitse toimintalinja", toimintalinja_opt
 
 # Filter Data based on selected programme
 if selected_toimintalinja != "All":
-#    data = data[data["Toimintalinja"] == selected_toimintalinja]
+    data = data[data["Toimintalinja"] == selected_toimintalinja]
 
 # Check if 'filter_ended' is in session state, if not initialize it
 if 'filter_ended' not in st.session_state:
@@ -72,6 +75,7 @@ if st.button("Piilota loppuneet projektit" if not st.session_state.filter_ended 
 
 # Generate the visualization
 if not data.empty:
-     generate_eura_project_viz(data, st.session_state.filter_ended)
+    generate_eura_project_viz(data, st.session_state.filter_ended)
 else:
-     st.write("No data found.")
+    st.write("No data found.")
+
