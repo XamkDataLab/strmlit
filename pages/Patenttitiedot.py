ef7d44d23fefcab08803fe738b5751c4355aaf4e
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-10-19 09:37:41 +0300

    Update Patenttitiedot.py

--- pages/Patenttitiedot.py
+++ pages/Patenttitiedot.py
@@ -1,1 +1,20 @@
 
+st.title("Patents Expiring Soon")
+
+df = fetch_patent_data()
+
+option = st.selectbox('Show patents expiring in the next:', ['3 months', '6 months', '12 months'])
+
+if option == '3 months':
+    max_date = datetime.today() + timedelta(days=90)
+elif option == '6 months':
+    max_date = datetime.today() + timedelta(days=180)
+else:
+    max_date = datetime.today() + timedelta(days=365)
+
+filtered_df = df[df['legal_status_anticipated_term_date'] <= max_date]
+
+# Display the filtered data
+st.write(f"Active Patents Expiring in the Next {option}:")
+st.table(filtered_df[['lens_id', 'invention_title']])
+
