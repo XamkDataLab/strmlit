a2fbcdb32de17df19d4e9e544b8da8b3f7627218
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-10-19 13:15:37 +0300

    Update Patenttitiedot.py

--- pages/Patenttitiedot.py
+++ pages/Patenttitiedot.py
@@ -8,6 +8,11 @@ from queries import *
 current_date = datetime.today().strftime('%Y-%m-%d')  
 st.title(f"Yritysten erääntyviä patentteja {current_date}")
 
+def transform_text(text):
+    if pd.isna(text):  # Check for NaN values
+        return None
+    return ' '.join([word.capitalize() for word in text.split()])
+
 df = fetch_legal_status_data()
 df['legal_status_anticipated_term_date'] = pd.to_datetime(df['legal_status_anticipated_term_date'])
 df = df[df['legal_status_anticipated_term_date'].notna()]
@@ -23,19 +28,14 @@ elif option == '6 months':
     max_date = min_date + timedelta(days=180)
 else:  # 12 months
     max_date = min_date + timedelta(days=365)
-
+    
 expiring_df = df[(df['legal_status_anticipated_term_date'] >= min_date) & (df['legal_status_anticipated_term_date'] <= max_date)]
+expiring_df['Hakija'] = expiring_df['yritys'].where(expiring_df['yritys'].notna(), expiring_df['extracted_name'].apply(transform_text))
 
-unique_applicants = expiring_df['extracted_name'].dropna().unique().tolist()
+unique_applicants = expiring_df['Hakija'].dropna().unique().tolist()
 selected_applicant = st.selectbox('Valitse hakija:', unique_applicants)
 
 filtered_df = expiring_df[expiring_df['extracted_name'] == selected_applicant]
-
-def transform_text(text):
-    if pd.isna(text):  # Check for NaN values
-        return None
-    return ' '.join([word.capitalize() for word in text.split()])
-
 filtered_df['Hakija'] = filtered_df['yritys'].where(filtered_df['yritys'].notna(), filtered_df['extracted_name'].apply(transform_text))
 
 st.write(f"Aktiiviset patentit hakijalta {selected_applicant} jotke erääntyvät seuraavan {option}:")
