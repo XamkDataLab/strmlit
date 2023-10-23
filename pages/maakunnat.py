540ce80b8fdde19b47b9d9fb18a4d2bc4796a771
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-10-24 01:09:34 +0300

    Update maakunnat.py

--- pages/maakunnat.py
+++ pages/maakunnat.py
@@ -3,26 +3,21 @@ import pandas as pd
 from queries import *
 
 def get_emblem_url_from_github(maakunta_name):
-    # Replace 'YourGitHubUsername' and 'YourRepoName' with your actual details
-    base_url = "https://raw.githubusercontent.com/YourGitHubUsername/YourRepoName/main/maakunta_emblems"
+    base_url = "https://github.com/XamkDataLab/strmlit/blob/main/vaakunat/"
     return f"{base_url}/{maakunta_name}.svg"
 
-# Fetch the data
+
 df = fetch_aggregated_data()
 
-# Create a unique list of Maakunnan_nimi values for the dropdown
 maakunnan_nimi_list = df['Maakunnan_nimi'].unique().tolist()
-maakunnan_nimi_list.insert(0, "All")  # Add an "All" option to show all data
+maakunnan_nimi_list.insert(0, "All")  
 
-# Create a dropdown menu and get the selected value
 selected_maakunnan_nimi = st.selectbox('Select Maakunnan_nimi:', maakunnan_nimi_list)
 
-# If a specific maakunta is selected (not "All"), display its emblem
 if selected_maakunnan_nimi != "All":
     emblem_url = get_emblem_url_from_github(selected_maakunnan_nimi)
     st.image(emblem_url, caption=f"Emblem for {selected_maakunnan_nimi}", use_column_width=True)
 
-    # Filter the dataframe based on the dropdown selection
     df = df[df['Maakunnan_nimi'] == selected_maakunnan_nimi]
 
 # Display the filtered dataframe
