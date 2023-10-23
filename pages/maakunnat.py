6fd964205afbda362e6f813f114326521c7082f1
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-10-24 01:38:27 +0300

    Update maakunnat.py

--- pages/maakunnat.py
+++ pages/maakunnat.py
@@ -13,7 +13,7 @@ maakunnan_nimi_list = df['Maakunnan_nimi'].unique().tolist()
 maakunnan_nimi_list.insert(0, "All")  
 
 selected_maakunnan_nimi = st.selectbox('Select Maakunnan_nimi:', maakunnan_nimi_list)
-col1, col2 = st.beta_columns([1,6])  # Adjust the numbers for desired column widths
+col1, col2 = st.columns([1,6])  # Adjust the numbers for desired column widths
 if selected_maakunnan_nimi != "All":
     emblem_url = get_emblem_url_from_github(selected_maakunnan_nimi)
     #st.image(emblem_url, caption=f"Emblem for {selected_maakunnan_nimi}", use_column_width=True)
