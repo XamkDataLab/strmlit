dee256280895a8156a7b29f8cf0a36fb38ad0382
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-10-24 09:25:19 +0300

    Update maakunnat.py

--- pages/maakunnat.py
+++ pages/maakunnat.py
@@ -16,9 +16,8 @@ selected_maakunnan_nimi = st.selectbox('Select Maakunnan_nimi:', maakunnan_nimi_
 col1, col2 = st.columns([1,6])  # Adjust the numbers for desired column widths
 if selected_maakunnan_nimi != "All":
     emblem_url = get_emblem_url_from_github(selected_maakunnan_nimi)
-    #st.image(emblem_url, caption=f"Emblem for {selected_maakunnan_nimi}", use_column_width=True)
     col1.image(emblem_url, caption=f"Emblem for {selected_maakunnan_nimi}", width=100)
-    st.write(emblem_url)
+   #st.write(emblem_url)
 
     df = df[df['Maakunnan_nimi'] == selected_maakunnan_nimi]
 
