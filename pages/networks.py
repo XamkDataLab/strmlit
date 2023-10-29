6f1c97a8f2a9cb4b5e3899c79f7ae9185557eae1
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-10-29 17:06:10 +0200

    Update networks.py

--- pages/networks.py
+++ pages/networks.py
@@ -5,10 +5,15 @@ from pyvis.network import Network
 import streamlit.components.v1 as components
 from queries import *
 
-data = fetch_collaboration_data()
+# Load the data
+@st.cache
+def load_data():
+    return pd.read_csv('/path/to/your/collab.csv')
+
+data = load_data()
 
 # Create a NetworkX graph
-@st.cache
+@st.cache_data()
 def create_graph():
     G = nx.Graph()
     for idx, row in data.iterrows():
@@ -21,42 +26,37 @@ G = create_graph()
 def filter_graph(graph, title=None, country=None, finnish_org=None):
     edges_to_keep = []
     for u, v, d in graph.edges(data=True):
-        if (title is None or d['title'] == title) and (country is None or d['country'] == country) and (finnish_org is None or u == finnish_org):
+        if (title is None or title == 'None' or d['title'] == title) and (country is None or country == 'None' or d['country'] == country) and (finnish_org is None or finnish_org == 'None' or u == finnish_org):
             edges_to_keep.append((u, v))
     filtered_graph = graph.edge_subgraph(edges_to_keep).copy()
     return filtered_graph
 
 # Visualization function
 def visualize_graph(graph):
-    nt = Network(notebook=False, height="500px", width="100%")
-    nt.from_nx(graph)
-    nt.save_graph("network.html")
-    with open("network.html", "r", encoding="utf-8") as f:
-        html = f.read()
-    components.html(html, height=500)
+    if graph.number_of_edges() > 0:
+        nt = Network(notebook=False, height="500px", width="100%")
+        nt.from_nx(graph)
+        nt.save_graph("network.html")
+        with open("network.html", "r", encoding="utf-8") as f:
+            html = f.read()
+        components.html(html, height=500)
+    else:
+        st.warning("No edges to display. Please select different filters.")
 
 # Streamlit app
 st.title('Collaboration Network Visualization')
 
-title = country = finnish_org = None
+titles = ['None'] + list(data['euroSciVocTitle'].unique())
+countries = ['None'] + list(data['CollaboratorCountry'].unique())
+finnish_orgs = ['None'] + list(data['FinnishOrgName'].unique())
 
-if st.checkbox('Filter by euroSciVocTitle'):
-    titles = data['euroSciVocTitle'].unique()
-    title = st.selectbox('Select Title', titles)
+title = st.selectbox('Filter by euroSciVocTitle', titles)
+country = st.selectbox('Filter by CollaboratorCountry', countries)
+finnish_org = st.selectbox('Filter by Finnish Organization', finnish_orgs)
+
+if title != 'None' or country != 'None' or finnish_org != 'None':
+    filtered_graph = filter_graph(G, title, country, finnish_org)
+    visualize_graph(filtered_graph)
 else:
-    st.warning('Select the checkbox to activate filtering by euroSciVocTitle')
+    st.warning('Please select at least one filter to visualize the network.')
 
-if st.checkbox('Filter by CollaboratorCountry'):
-    countries = data['CollaboratorCountry'].unique()
-    country = st.selectbox('Select Country', countries)
-else:
-    st.warning('Select the checkbox to activate filtering by CollaboratorCountry')
-
-if st.checkbox('Filter by Finnish Organization'):
-    finnish_orgs = data['FinnishOrgName'].unique()
-    finnish_org = st.selectbox('Select Finnish Organization', finnish_orgs)
-else:
-    st.warning('Select the checkbox to activate filtering by Finnish Organization')
-
-filtered_graph = filter_graph(G, title, country, finnish_org)
-visualize_graph(filtered_graph)
