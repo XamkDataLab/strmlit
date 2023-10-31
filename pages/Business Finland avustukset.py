ef179dbd8320fcb4f53274a88d71f3c821a5e581
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-10-31 11:08:24 +0200

    Update Business Finland avustukset.py

--- pages/Business Finland avustukset.py
+++ pages/Business Finland avustukset.py
@@ -10,6 +10,7 @@ def get_emblem_url_from_github(maakunta_name):
 
 df = fetch_aggregated_data()
 df = df[df['Maakunnan_nimi'].notna()]
+st.dataframe(df)
 maakunnan_nimi_list = df['Maakunnan_nimi'].unique().tolist()
 maakunnan_nimi_list.insert(0, "All")  
 st.dataframe(df)
