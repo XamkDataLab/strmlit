a41b78fa43c4423ed13dabde706cca780d7d61b4
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-10-31 12:48:26 +0200

    Update queries.py

--- queries.py
+++ queries.py
@@ -426,4 +426,6 @@ def fetch_data2(y_tunnus):
     
     # Connect to the database and fetch the data into a Pandas DataFrame
     with pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}') as conn:
-        df = pd.read_sql(query, conn, params=(y_tunnus, y_tunnus, y_tunnus, y_tunnus, y
+           df = pd.read_sql(query, conn, params=(y_tunnus, y_tunnus, y_tunnus, y_tunnus, y_tunnus, y_tunnus, y_tunnus))
+        
+    return df
