a97d3485b64e2d73b3b6dd9e1ca77953de3c95fd
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-11-02 11:43:48 +0200

    Update network2.py

--- pages/network2.py
+++ pages/network2.py
@@ -18,15 +18,7 @@ def create_graph(data):
 
 G = create_graph(data)
 
-def filter_data(data, title=None, country=None, finnish_org=None):
-    filtered_data = data.copy()
-    if title and title != 'None':
-        filtered_data = filtered_data[filtered_data['euroSciVocTitle'] == title]
-    if country and country != 'None':
-        filtered_data = filtered_data[filtered_data['CollaboratorCountry'] == country]
-    if finnish_org and finnish_org != 'None':
-        filtered_data = filtered_data[filtered_data['FinnishOrgName'] == finnish_org]
-    return filtered_data
+# Rest of your code remains the same
 
 def visualize_graph(graph):
     if graph.number_of_edges() > 0:
@@ -41,33 +33,32 @@ def visualize_graph(graph):
             nt.add_edge(u, v)
         
         # Set options to include a callback for node click
-        nt.set_options("""
-        var options = {
-          interaction: {hover:true},
-          nodes: {
-            borderWidth: 2,
-            size: 30,
-            color: {
-              border: '#222222',
-              background: '#666666'
+        network_options = {
+            "interaction": {"hover": True},
+            "nodes": {
+                "borderWidth": 2,
+                "size": 30,
+                "color": {
+                    "border": "#222222",
+                    "background": "#666666"
+                },
+                "font": {"color": "#eeeeee"}
             },
-            font:{color:'#eeeeee'}
-          },
-          edges: {
-            color: 'lightgray'
-          }
+            "edges": {
+                "color": "lightgray"
+            }
         }
-        """)
+        nt.set_options(network_options)
         
         # Save and display the graph
         nt.save_graph("network.html")
         with open("network.html", "r", encoding="utf-8") as f:
-            html = f.read().replace('var options = {};', 'var options = network_options;')  # Use the options defined above
+            html = f.read()
         components.html(html, height=500)
     else:
         st.warning("No edges to display. Please select different filters.")
-      
-# Streamlit app
+
+# Rest of your Streamlit app remains the same
 st.title('Horizon Europe yhteistyöverkostokaavio')
 st.text('Hankekonsortiot joissa mukana vähintään yksi suomalainen yritys.') 
 
@@ -84,4 +75,5 @@ if title != 'None' or country != 'None' or finnish_org != 'None':
     filtered_graph = create_graph(filtered_data)
     visualize_graph(filtered_graph)
 else:
-    st.warning('Valitse vähintään yksi suodatin luodaksesi verkostokaavion.') 
+    st.warning('Valitse vähintään yksi suodatin luodaksesi verkostokaavion.')
+
