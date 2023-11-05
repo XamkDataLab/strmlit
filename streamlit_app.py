4ad3e1598228ddf2a15466d514f44ba04318bace
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-11-05 23:24:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,11 @@
 import streamlit as st
 import urllib.parse
 
-# Function to get the URL of an emblem from GitHub, encoding the filename to handle special characters
 def get_emblem_url_from_github(maakunta_name):
     base_url = "https://raw.githubusercontent.com/XamkDataLab/strmlit/main/vaakunat"
-    # Ensure the filename is properly encoded to handle special characters like ä or ö
     encoded_maakunta_name = urllib.parse.quote(maakunta_name)
     return f"{base_url}/{encoded_maakunta_name}.svg"
 
-# List of actual maakunta names
 maakunta_names = [
     "Ahvenanmaa",
     "Etelä-Karjala",
@@ -31,19 +28,16 @@ maakunta_names = [
     "Varsinais-Suomi"
 ]
 
-# Display the emblem ribbon
-# Adjust the number of columns based on the number of emblems to display per row
 cols = st.columns(len(maakunta_names))
 for idx, maakunta_name in enumerate(maakunta_names):
     with cols[idx]:
         emblem_url = get_emblem_url_from_github(maakunta_name)
-        # Set a fixed width for the images to make them smaller
-        st.image(emblem_url, width=50)  # Adjust the width as needed
+        st.image(emblem_url, width=50) 
 
-# Main title and introduction
-st.title("Innovaatioiden ja hankerahoituksen Suomi")
 
-st.subheader("Mistä on kysymys")
+st.title("Ipr-Suomi")
+
+st.subheader("Maakuntien innovaatiotilastot")
 st.write(
     """
     Tämä sovellus kerää yhteen tiedot suomalaisten yritysten IPR-omaisuudesta ja rahoituksesta. Sisältää
@@ -51,8 +45,8 @@ st.write(
     """
 )
 st.markdown("""
-- Maakuntasivut: tarkastele maakunnan IPR-aktiivisuutta ja vahvuusaloja
-- Yrityshaku: hae yrityksen rahoitustiedot.
-- Verkosto
-- erääntyvät patentit
+- Maakuntasivut: tarkastele maakunnan vahvuusaloja ja yrityksiä 
+- Yrityshaku: hae yrityskohtaiset tiedot ja organisaatioiden hankesalkut
+- Verkosto: tarkastele yhteishankkeita maakuntien välillä
+- Erääntyvät patentit :)
 """)
