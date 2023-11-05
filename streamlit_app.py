e0a5d386d9d3ba2b5aad9b2979aa2c6120529dff
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-11-05 13:45:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,13 +32,16 @@ maakunta_names = [
 ]
 
 # Display the emblem ribbon
-st.title("Welcome to the Regional Emblem App")
-st.header("Regional Emblems")
-cols = st.columns(3)  # Adjust the number of columns as needed for layout
+# Adjust the number of columns based on the number of emblems to display per row
+cols = st.columns(len(maakunta_names))
 for idx, maakunta_name in enumerate(maakunta_names):
-    with cols[idx % 3]:  # This will cycle through the columns
+    with cols[idx]:
         emblem_url = get_emblem_url_from_github(maakunta_name)
-        st.image(emblem_url, use_column_width=True, caption=maakunta_name)
+        # Set a fixed width for the images to make them smaller
+        st.image(emblem_url, width=50, caption=maakunta_name)  # Adjust the width as needed
+
+# Main title and introduction
+st.title("Welcome to the Regional Emblem App")
 
 st.subheader("About the App")
 st.write(
