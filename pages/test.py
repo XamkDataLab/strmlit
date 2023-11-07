5dd749c45328dfd7b19e977e4acc4f71ec2c425e
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-11-07 10:01:45 +0200

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -3,12 +3,17 @@ from queries import *
 
 st.title('Hae yrityksen tiedot')
 
-# Fetch and display the selectbox with company names
+# Fetch the company names for the selectbox
 company_names = get_company_names()
+
+# Use a placeholder for the company data to display it later
+company_data_placeholder = st.empty()
+
+# Create a selectbox for user to choose the company
 selected_company = st.selectbox('Valitse yritys', company_names)
 
-# When a company is selected, fetch and display the data
-if selected_company:
+# Button to trigger the data fetching
+if st.button('Hae tiedot'):
+    # When the button is clicked, fetch and display the data for the selected company
     data = fetch_data3(selected_company)
-    st.write(data)
-
+    company_data_placeholder.write(data)
