07f9c3eb3eb0a1202f7581dfc1ed939305bea26c
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-11-07 12:52:45 +0200

    Update 2yrityshaku_aikasarjat.py

--- pages/2yrityshaku_aikasarjat.py
+++ pages/2yrityshaku_aikasarjat.py
@@ -8,9 +8,37 @@ y_tunnus = st.session_state.get('y_tunnus')
 st.title(f"Otsikko yritykselle {y_tunnus}")
 
 if y_tunnus:
-    data = fetch_company_data(y_tunnus)
+    patents_data, trademarks_data = fetch_time_series_data(y_tunnus)
+
+    # Display the patents data
+    st.subheader("Patent Data")
+    st.dataframe(patents_data)
+
+    # Display the trademarks data
+    st.subheader("Trademark Data")
+    st.dataframe(trademarks_data)
+
+    # You can also plot the data if they have a time component
+    # Assuming the dates are in 'event_date' column for both patents and trademarks
+    # and you want to plot the counts of patents and trademarks over time
+
+    # Combine the dataframes for plotting
+    combined_data = pd.concat([
+        patents_data.assign(type='Patent'),
+        trademarks_data.assign(type='Trademark')
+    ])
+
+    # Plotting
+    if not combined_data.empty:
+        # Convert event_date to datetime if not already
+        combined_data['event_date'] = pd.to_datetime(combined_data['event_date'])
+        combined_data = combined_data.sort_values('event_date')
+
+        # Create a plot with Plotly Express
+        fig = px.line(combined_data, x='event_date', color='type', markers=True,
+                      title='Patents and Trademarks Over Time')
+
+        # Display the plot
+        st.plotly_chart(fig)
 else:
     st.write("Invalid or missing parameters.")
-    
-st.dataframe(data)
-
