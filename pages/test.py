32a8df73f4ffd815921fc63eca61c8850f13fda4
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-11-07 09:51:32 +0200

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -3,17 +3,10 @@ from queries import *
 
 st.title('Hae yrityksen tiedot')
 
-# Input for search
-search_term = st.text_input("Anna Y-tunnus tai yrityksen nimi (ja paina enter)")
-search_type = st.radio("Valitse hakutyyppi:", ('y_tunnus', 'company_name'))
+company_names = get_company_names()
+    selected_company = st.selectbox('Select a company', company_names)
 
-if search_term:
-  
-    data = fetch_data_new(search_term, search_by=search_type)
-    
-    # Check if the data is not empty
-    if not data.empty:
-        st.write(f"Tulokset hakutermin '{search_term}' mukaan:")
-        st.dataframe(data)  # Display the DataFrame in the app
-    else:
-        st.write("Ei tuloksia hakuehdoilla.")
+f selected_company:
+        data = fetch_data_by_company_name(selected_company)
+        st.write(data)
+
