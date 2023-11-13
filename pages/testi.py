f9c2c426980d50b65c215d1f09b0a2c4f64ba89e
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-11-13 12:13:51 +0200

    Update testi.py

--- pages/testi.py
+++ pages/testi.py
@@ -4,12 +4,22 @@ import numpy as np
 import plotly.graph_objects as go
 from queries import *
 
-def make_cpc(df, cpc_json_file):
+json_url = "https://raw.githubusercontent.com/XamkDataLab/strmlit/main/cpc_ultimate_titles.json"
+
+def breakdown_cpc(code):
+    section = code[0]
+    c_class = code[:3]
+    subclass = code[:4]
+    group = code.split('/')[0]
+    subgroup = code
+    return pd.Series([section, c_class, subclass, group, subgroup])
+
+def make_cpc(df):
     
-    cpc = pd.read_json(cpc_json_file)
+    cpc = pd.read_json(json_url)
     df[['Section', 'Class', 'Subclass', 'Group', 'Subgroup']] = df['cpc_classification'].apply(breakdown_cpc)
     df['Group'] = df['Group'].apply(lambda x: x + "/00")
-    df.drop(['cpc_code_split', 'class'], axis=1, inplace=True)
+    #df.drop(['cpc_code_split', 'class'], axis=1, inplace=True)
     df['Section Description'] = df['Section'].map(cpc.set_index('Code')['Description'])
     df['Class Description'] = df['Class'].map(cpc.set_index('Code')['Description'])
     df['Subclass Description'] = df['Subclass'].map(cpc.set_index('Code')['Description'])
@@ -18,11 +28,12 @@ def make_cpc(df, cpc_json_file):
 
     return df
 
-
 y_tunnus = st.session_state.get('y_tunnus')
 yritys_nimi = st.session_state.get('yritys')
 
 if y_tunnus:
-        cpc_data = fetch_company_cpc_data(y_tunnus)
+    cpc_data = fetch_company_cpc_data(y_tunnus)
+    cpc_data = cpc_data[make_cpc]
+    
   
         
