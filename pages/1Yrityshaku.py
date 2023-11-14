424cd866b0a54ccfb818d1fb690550e1c4daf6c4
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-11-14 22:29:51 +0200

    Update 1Yrityshaku.py

--- pages/1Yrityshaku.py
+++ pages/1Yrityshaku.py
@@ -233,7 +233,8 @@ if y_tunnus:
         
         # In a Streamlit app, use st.plotly_chart() to display the plot
         st.plotly_chart(fig)
-        
+
+    if y_tunnus:
         cpc_data2 = cpc_data(make_cpc)
         cpc2_class_counts = cpc_data2['Class description'].value_counts()
 
@@ -242,5 +243,5 @@ if y_tunnus:
                      y=cpc2_class_counts.values,
                      labels={'x': 'Class', 'y': 'Number of Classifications'},
                      title='Distribution of Patents Across CPC Classes')
-                     st.plotly_chart(fig2)
+        st.plotly_chart(fig2)
         
