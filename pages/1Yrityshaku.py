525e74768d9f081a8a1b684b5c5d870a9ab109df
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-11-14 23:28:20 +0200

    Update 1Yrityshaku.py

--- pages/1Yrityshaku.py
+++ pages/1Yrityshaku.py
@@ -272,7 +272,7 @@ if y_tunnus:
         
         # In a Streamlit app, use st.plotly_chart() to display the plot
         st.plotly_chart(fig)
-        cpc_data = cpc_data[cpc_data['cpc_classification'].isna() | df['ColumnA'].str.len() >= 4]
+        cpc_data = cpc_data[cpc_data['cpc_classification'].isna() | df['cpc_classification'].str.len() >= 4]
         cpc_data2 = make_cpc(cpc_data)
         st.dataframe(cpc_data2)
         fig_class = create_bar_chart(cpc_data2, 'Class Description', 'Frequency of Class Descriptions', 'Class Description')
