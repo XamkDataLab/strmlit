5eec0bbbaa9a20387ed4c5ff5d1f375ab76cf137
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-12-08 13:01:48 +0200

    Update Nimihaku.py

--- pages/Nimihaku.py
+++ pages/Nimihaku.py
@@ -1,21 +1,24 @@
 import streamlit as st
 from queries import *
 
-st.title('Hae yrityksen tiedot')
-
-all_company_names = get_company_names()
+all_company_data = get_company_names2()
 
 search_query = st.text_input('Hae yritystä')
 
-filtered_company_names = [name for name in all_company_names if search_query.lower() in name.lower()]
+# Filter company names based on the search query
+filtered_company_names = all_company_data[all_company_data['yritys'].str.lower().str.contains(search_query.lower())]
 
 if search_query:
-    selected_company = st.selectbox('Valitse yritys', filtered_company_names)
+    selected_company = st.selectbox('Valitse yritys', filtered_company_names['yritys'])
 else:
-    selected_company = st.selectbox('Valitse yritys', all_company_names[:50])  # Show only the first 50 names or some limited number
+    # Show only the first 50 names or some limited number
+    selected_company = st.selectbox('Valitse yritys', all_company_data['yritys'][:50])
 
 if st.button('Hae tiedot'):
     if selected_company:
-        data = fetch_individual_data(selected_company)
+        # Retrieve the y_tunnus for the selected company
+        y_tunnus = filtered_company_names[filtered_company_names['yritys'] == selected_company]['y_tunnus'].iloc[0]
+        data = fetch_individual_data(y_tunnus)
         st.write(data)
+
 
