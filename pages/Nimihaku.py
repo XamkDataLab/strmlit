03ecc42be5db449973b98e403363f7353dc386d1
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2023-12-08 13:10:48 +0200

    Update Nimihaku.py

--- pages/Nimihaku.py
+++ pages/Nimihaku.py
@@ -1,24 +1,30 @@
 import streamlit as st
 from queries import *
 
+# Load company data
 all_company_data = get_company_names2()
 
+# Input for search query
 search_query = st.text_input('Hae yritystä')
 
-# Filter company names based on the search query
-filtered_company_names = all_company_data[all_company_data['yritys'].str.lower().str.contains(search_query.lower())]
-
 if search_query:
-    selected_company = st.selectbox('Valitse yritys', filtered_company_names['yritys'])
+    # Filter company names based on the search query
+    filtered_company_names = all_company_data[all_company_data['yritys'].str.lower().str.contains(search_query.lower())]
+    dropdown_options = filtered_company_names['yritys']
 else:
-    # Show only the first 50 names or some limited number
-    selected_company = st.selectbox('Valitse yritys', all_company_data['yritys'][:50])
+    # Show only the first 50 names or some limited number if no search query
+    dropdown_options = all_company_data['yritys'][:50]
 
-if st.button('Hae tiedot'):
-    if selected_company:
+selected_company = st.selectbox('Valitse yritys', dropdown_options)
+
+if st.button('Hae tiedot') and selected_company:
+    # Check if the selected company is in the filtered list
+    if selected_company in filtered_company_names['yritys'].values:
         # Retrieve the y_tunnus for the selected company
         y_tunnus = filtered_company_names[filtered_company_names['yritys'] == selected_company]['y_tunnus'].iloc[0]
         data = fetch_individual_data(y_tunnus)
         st.write(data)
+    else:
+        st.error("Valittua yritystä ei löytynyt.")
 
 
